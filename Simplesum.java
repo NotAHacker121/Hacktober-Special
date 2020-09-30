c71e3b380c25cc7928f0e239a6965f88de543d57
NotAHacker121 <56961789+NotAHacker121@users.noreply.github.com>
2020-09-30 16:51:01 +0530

    Added comments

--- Simplesum.java
+++ Simplesum.java
@@ -1,9 +1,9 @@
 //My first java contribution
 
 public class Simplesum {
-  public static void main(String args[]) {
-    int a = 5;
-    int b = 10;
-    System.out.println("Sum = " + a+b);
+  public static void main(String args[]) {      //declaring main
+    int a = 5;                                  //initializing a
+    int b = 10;                                 //initializing b
+    System.out.println("Sum = " + a+b);         //printing sum of a and b
   }
 }
